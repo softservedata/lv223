ba3ce9f76b88001b587a2d6dd22dd07cf644e330
lifarenyuk <lifarenyuk@gmail.com>
2017-03-30 13:38:18 +0300

    Added functionality related to right menu

---
+++ src/test/java/com/softserve/edu/opencart/pages/EditAccountPage.java
@@ -0,0 +1,14 @@
+package com.softserve.edu.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by FPF on 30.03.2017.
+ */
+public class EditAccountPage extends ARightMenuRegistered {
+
+
+    protected EditAccountPage(WebDriver driver) {
+        super(driver);
+    }
+}
