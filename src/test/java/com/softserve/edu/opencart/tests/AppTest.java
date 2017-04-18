170233b7c2e5874fc1ab05afd6dfe5cbea0ac09e
yaroslav <garasym@gmail.com>
2017-04-18 14:22:27 +0300

    Fixed ProductTest. Comment ASortingPage

---
+++ src/test/java/com/softserve/edu/opencart/tests/AppTest.java
@@ -0,0 +1,41 @@
+package com.softserve.edu.opencart.tests;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	// ublic Calendar c;
+
+	@BeforeMethod
+	public void setUp() throws Exception {
+		System.out.println("CalcTest @Before setUp()");
+	}
+
+	@AfterMethod
+	public void tearDown() throws Exception {
+		System.out.println("CalcTest @After tearDown()");
+		Reporter.log("<BR><FONT SIZE='4' COLOR='RED'>@AfterMethod Non Conditional.</FONT><BR>", true);
+		Reporter.log("<BR><FONT SIZE='4' COLOR='BLUE'>@AfterMethod Level 3</FONT><BR>", 3, true);
+		Reporter.log("<BR><FONT SIZE='4' COLOR='BLUE'>@AfterMethod Level 5</FONT><BR>", 5);
+		Reporter.log("<BR><FONT SIZE='4' COLOR='BLUE'>@AfterMethod Level 7</FONT><BR>", 7, true);
+	}
+
+	@Test
+	public void testApp() {
+		System.out.println("***@Test Report MAVEN  " + System.getProperty("surefire.reports.directory"));
+		Reporter.log("<BR><FONT SIZE='4' COLOR='RED'>Non Conditional.</FONT><BR>", true);
+		Reporter.log("<BR><FONT SIZE='4' COLOR='BLUE'>Level 3</FONT><BR>", 3, true);
+		Reporter.log("<BR><FONT SIZE='4' COLOR='BLUE'>Level 5</FONT><BR>", 5);
+		Reporter.log("<BR><FONT SIZE='4' COLOR='BLUE'>Level 7</FONT><BR>", 7, true);
+		System.out.println("Running . . .");
+		// ReporterWrapper.get().debug("DEBUG - messages");
+		// ReporterWrapper.get().info("INFO - messages");
+		// ReporterWrapper.get().warning("WARNING - messages");
+		// ReporterWrapper.get().error("ERROR - messages");
+		Assert.assertTrue(true);
+	}
+
+}
