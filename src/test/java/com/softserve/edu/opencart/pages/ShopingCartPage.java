318890e20bd8ae457efa7137c56ace2464bd19d7
orlann <orlann30@gmail.com>
2017-04-21 13:25:52 +0300

    Start ShopingCartPage

--- src/test/java/com/softserve/edu/opencart/pages/ShopingCartPage.java
+++ src/test/java/com/softserve/edu/opencart/pages/ShopingCartPage.java
@@ -1,13 +1,22 @@
 package com.softserve.edu.opencart.pages;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class ShopingCartPage {	
-
+public class ShopingCartPage extends ATopPage{
+	
+	private List<CartListComponent> shopingCartList;
+	
+	
+	public ShopingCartPage(WebDriver driver) {
+		super(driver);
+	//	this.shopingCartList=ATopPage.getComponents();
+	}
+	
+	
 	
 }
