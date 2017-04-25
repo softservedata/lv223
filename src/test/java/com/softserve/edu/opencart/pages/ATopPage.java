b5afda6cb305eaca742837d974529b0bd208e816
yaroslav <garasym@gmail.com>
2017-04-25 14:53:56 +0300

    update

--- src/test/java/com/softserve/edu/opencart/pages/ATopPage.java
+++ src/test/java/com/softserve/edu/opencart/pages/ATopPage.java
@@ -1,14 +1,15 @@
 package com.softserve.edu.opencart.pages;
 
-import com.softserve.edu.opencart.tools.GeneralException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.softserve.edu.opencart.tools.GeneralException;
 
 abstract class ATopPage {
 
